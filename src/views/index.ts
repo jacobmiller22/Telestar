f1998c1c7ec432b5635a7089a2595bf2de44a7eb
jacobmiller22 <jacobmiller22@vt.edu>
2021-12-19 13:06:47 -0500

    Fixed export error

--- src/views/index.ts
+++ src/views/index.ts
@@ -1,1 +1,4 @@
-export { default as IndexView } from "./IndexView";
+export { default as IndexView } from "./Dashboard";
+export { default as Inventory } from "./Inventory";
+export { default as GoalNew } from "./GoalNew";
+export { default as Bills } from "./Bills";
